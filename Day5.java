3430fbff7199ca6f411ed0d2c32752526af1af86
Manpreet Singh <manpreetsingh0001ms98@gmail.com>
2020-10-14 13:15:10 +0530

    Day5

---
+++ Day5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution{
+    public static void main(String[] ds){
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+
+        for( int i =1;i<=10;i++){
+            System.out.println(size+" x "+i+" = "+size*i);
+        }
+    }
+}
